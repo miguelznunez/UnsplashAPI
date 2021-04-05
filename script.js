df08571e0345c725e4c8f5f061b78338e5ae29e5
Miguel Nunez <71807189+iamcodefoxx@users.noreply.github.com>
2021-04-05 09:43:09 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,60 @@
+const input = document.getElementById("input");
+const list = document.getElementsByClassName("grid")[0];
+
+window.addEventListener('load', dayNightMode);
+
+input.addEventListener("keydown", function(event) {
+  if (event.key == "Enter")
+    loadImg();
+});
+
+function loadImg(){
+
+  removeImages();
+
+  const url = 'https://api.unsplash.com/search/photos?query='+input.value+'&per_page=9&client_id=SouHY7Uul-OxoMl3LL3c0NkxUtjIrKwf3tsGk1JaiVo';
+
+  fetch(url)
+
+  .then(response => {
+      if(response.ok)
+        return response.json();
+      else
+        console.log(response.status);
+  })
+
+  .then(data => { 
+    createImagesArray(data);
+  });    
+}
+
+function createImagesArray(data){
+  const imageNodes = [];
+  for(let i = 0;i < data.results.length;i++){
+    imageNodes[i] = document.createElement("div");
+    imageNodes[i].className = "img";
+    imageNodes[i].style.backgroundImage = "url("+data.results[i].urls.raw+")";
+    imageNodes[i].addEventListener("dblclick", function(){
+      window.open(data.results[i].links.download, '_blank');
+    })
+    list.appendChild(imageNodes[i]);
+  }
+}
+
+function removeImages(){
+  list.innerHTML = '';
+}
+
+function dayNightMode(){
+  const date = new Date();
+  const hour = date.getHours();
+
+  if(hour >= 7 && hour <= 19){
+    document.body.style.backgroundColor = 'whitesmoke';
+    document.body.style.color = 'black';
+  }
+  else{
+    document.body.style.backgroundColor = 'black';
+    document.body.style.color = 'white';
+  }
+}
